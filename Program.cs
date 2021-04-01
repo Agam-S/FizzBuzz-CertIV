9cdf28f0724e7bbabd6d1ffd41d53e5d0daa8621
Agam Singh <79820189+Agam-S@users.noreply.github.com>
2021-04-02 00:10:29 +1100

    first

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        for (int counter = 1;
+        counter <= 100; counter++)
+
+        if (counter % 3 == 0 && counter % 5 == 0) {
+
+        System.Console.WriteLine("FizzBuzz");
+        }
+
+        else if (counter % 3 == 0) {
+        System.Console.WriteLine("Fizz");
+        }
+        
+        else if (counter % 5 == 0) {
+        System.Console.WriteLine("Buzz");
+       }
+
+        else {
+        System.Console.WriteLine(counter);
+        }
+       }
+    }
+}
